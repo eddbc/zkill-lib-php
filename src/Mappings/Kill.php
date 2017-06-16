26dc4edeb25b25ba4db29538deab0edb9bc7a2fa
Edward Billington-Cliff <yddebc@gmail.com>
2017-06-16 13:20:07 +0100

    add toString to Kill

--- src/Mappings/Kill.php
+++ src/Mappings/Kill.php
@@ -29,5 +29,15 @@ class Kill {
 	/** @var ZKB $zkb */
 	public $zkb;
 
+    /**
+     * Returns the kill ID as a string.
+     * Only intended to be used for comparing Kill objects by built-in PHP functions etc
+     * 
+     * @return string
+     */
+	public function __toString()
+    {
+        return (string) $this->killID;
+    }
 
 }
